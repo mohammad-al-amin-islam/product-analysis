95e97b8d1b50b2b1d4fe46cbb8a46e0c912cf635
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-04 10:13:04 +0600

    header and other components added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='flex justify-center my-10'>
+                <CustomLink className='mr-6 text-xl font-bold' to='/home'>Home</CustomLink>
+                <CustomLink className='mr-6 text-xl font-bold' to='/reviews'>Reviews</CustomLink>
+                <CustomLink className='mr-6 text-xl font-bold' to='/dashboard'>Dashboard</CustomLink>
+                <CustomLink className='mr-6 text-xl font-bold' to='/blogs'>Blogs</CustomLink>
+                <CustomLink className='mr-6 text-xl font-bold' to='/about'>About</CustomLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
