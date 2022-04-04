04607bc6d9f102fb6ebbbee9fdf33bdea54fd472
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-04 15:53:57 +0600

    chart added to  dash board

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import SimpleAreaChart from '../SimpleAreaChart/SimpleAreaChart';
+import SimpleLineChart from '../SimpleLineChart/SimpleLineChart';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>This is Dashboard</h1>
+        <div className='flex justify-between'>
+            <div className='flex flex-col justify-center'>
+                <h1 className='text-3xl text-center font-bold text-blue-500 mb-6'>Month Wise Sell</h1>
+                <SimpleLineChart></SimpleLineChart>
+            </div>
+            <div className='w-1/2 mt-10'>
+                <h1 className='text-3xl text-center font-bold text-blue-500 mb-6'>Invesment Vs Revenue</h1>
+                <SimpleAreaChart></SimpleAreaChart>
+            </div>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import CustomLink from '../CustomLink/CustomLink';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <nav className='flex justify-center p-8 uppercase shadow'>
-                <CustomLink className='mr-6 text-xl font-bold' to='/home'>Home</CustomLink>
-                <CustomLink className='mr-6 text-xl font-bold' to='/reviews'>Reviews</CustomLink>
-                <CustomLink className='mr-6 text-xl font-bold' to='/dashboard'>Dashboard</CustomLink>
-                <CustomLink className='mr-6 text-xl font-bold' to='/blogs'>Blogs</CustomLink>
-                <CustomLink className='mr-6 text-xl font-bold' to='/about'>About</CustomLink>
+        <div className='flex justify-between items-center shadow font-bold text-3xl '>
+            <div className='ml-5 uppercase'>
+                <Link to='/'>Love Readings</Link>
+            </div>
+
+            <nav className='flex justify-between p-8 uppercase'>
+
+                <CustomLink className='mr-6 text-lg font-bold' to='/home'>Home</CustomLink>
+                <CustomLink className='mr-6 text-lg font-bold' to='/reviews'>Reviews</CustomLink>
+                <CustomLink className='mr-6 text-lg font-bold' to='/dashboard'>Dashboard</CustomLink>
+                <CustomLink className='mr-6 text-lg font-bold' to='/blogs'>Blogs</CustomLink>
+                <CustomLink className='mr-6 text-lg font-bold' to='/about'>About</CustomLink>
             </nav>
         </div>
     );
