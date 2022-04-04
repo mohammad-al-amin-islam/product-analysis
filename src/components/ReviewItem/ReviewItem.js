f16c048291fe4c0accf7d47cf69243acec707cbc
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-04 11:34:16 +0600

    review added

---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ReviewItem = ({ item }) => {
+    const { id, name, img, givenReview, review } = item;
+    return (
+        <div className='w-3/4 m-7 mx-auto rounded h-full bg-slate-100 p-3 shadow-md'>
+            <div className='flex items-center'>
+                <div className='w-14 h-14 mr-3'>
+                    <img src={img} alt="" />
+                </div>
+                <div className='text-left'>
+                    <h1 className='text-2xl te'>{name}</h1>
+                    <p className='font-bold'>Given Ratings: {review}</p>
+                </div>
+            </div>
+            <p className='text-left'>{givenReview}</p>
+
+        </div>
+    );
+};
+
+export default ReviewItem;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import useReviews from '../../hooks/useReviews';
+import ReviewItem from '../ReviewItem/ReviewItem';
 const Reviews = () => {
+    const [reviews, setReviews] = useReviews();
     return (
         <div>
-            <h1>This is Reviews</h1>
+            <h1 className='text-3xl'>All Reviews: {reviews.length}</h1>
+            {
+                reviews.map(review => <ReviewItem
+                    key={review.id}
+                    item={review}
+                ></ReviewItem>)
+            }
         </div>
     );
 };
